bc18cf5d054426c5702b2b46ddce599e1d50a50a
Gagan Chohan <gaganchohan@Gagans-MacBook-Air.local>
2021-12-11 15:22:49 +1100

    Added basic terraforn file and READ ME file

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "rg-yield-dev" {
+  name     = "rg-yield-dev"
+  location = "Australia Southeast"
+}
