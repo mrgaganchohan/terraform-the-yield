639d3878df2b28f1f0dc1b950c4c103d16883b22
Gagan Chohan <gaganchohan@Gagans-MacBook-Air.local>
2021-12-11 23:49:18 +1100

    Added app Service plan and app service

--- modules/common_resources/main.tf
+++ modules/common_resources/main.tf
@@ -1,6 +1,18 @@
 # Create a resource group
 resource "azurerm_resource_group" "rg-yield-dev" {
   name     = "rg-yield-app"
-  location = "Australia Southeast"
+  location = var.location
 }
 
+resource "azurerm_app_service_plan" "plan-yield-app" {
+  name                = "plan-yield-app"
+  location            = var.location
+  kind                = "Linux"
+  reserved = true
+  resource_group_name = azurerm_resource_group.rg-yield-dev.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
---
+++ modules/frontend/main.tf
@@ -0,0 +1,11 @@
+resource "azurerm_app_service" "app-service-yield-frontend" {
+  name                = "app-service-yield-frontend"
+  location            = var.location
+  resource_group_name = var.rg_name
+  app_service_plan_id = var.service_plan_yield_id
+
+  site_config {
+    linux_fx_version = "node|14-lts"
+  }
+}
+
